1a01087868ed2b9a24d654fa1e83db9545e7ee13
Reyth3 <mrreynevan2@gmail.com>
2017-07-29 10:43:34 +0200

    API Wrapper: Added Support for "api/events" Endpoint

---
+++ JNCReaderUWP/ViewModel/HamburgerMenuItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JNCReaderUWP.ViewModel
+{
+    public class HamburgerMenuItemViewModel
+    {
+        public HamburgerMenuItemViewModel() { }
+        public HamburgerMenuItemViewModel(char glyph, string title, Type pageType, Action secondaryAction = null)
+        {
+            Glyph = glyph;
+            Title = title;
+            PageType = pageType;
+            SecondaryAction = secondaryAction;
+        }
+
+        public HamburgerMenuItemViewModel(char glyph, string title, Action secondaryAction)
+        {
+            Glyph = glyph;
+            Title = title;
+            PageType = null;
+            SecondaryAction = secondaryAction;
+        }
+
+        public char Glyph { get; set; }
+        public string Title { get; set; }
+        public Type PageType { get; set; }
+        public Action SecondaryAction { get; set; }
+    }
+}
---
+++ JNovelClub/Api/EventsAPI.cs
@@ -0,0 +1,29 @@
+﻿using JNovelClub.Helpers;
+using JNovelClub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JNovelClub.Api
+{
+    class EventsAPI
+    {
+        public EventsAPI(JNClient client)
+        {
+            _client = client;
+        }
+
+        private JNClient _client;
+
+        public async Task<Response<Event[]>> GetListOfEvents()
+        {
+            using (var http = _client._apiClient)
+            using (var res = await http.GetAsync("api/events"))
+            {
+                return new Response<Event[]>(await res.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
