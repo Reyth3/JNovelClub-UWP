e2022812f2fbad41e6838b9bdd67bdb5b5d68f18
Reyth3 <mrreynevan2@gmail.com>
2017-07-31 01:37:49 +0200

    Added Novel Details Page

---
+++ JNCReaderUWP/View/LightNovels.xaml.cs
@@ -0,0 +1,46 @@
+﻿using JNCReaderUWP.ViewModel;
+using JNovelClub;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace JNCReaderUWP.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LightNovels : Page
+    {
+        public LightNovels()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            pw.IsActive = true;
+            var jnc = new JNClient();
+            var seriesResponse = await jnc.Series.GetListOfSeries();
+            DataContext = seriesResponse.Result.OrderBy(o => o.Title).Select(o => new SeriesViewModel(o));
+            pw.IsActive = false;
+        }
+
+        private void NovelClicked(object sender, ItemClickEventArgs e)
+        {
+            Frame.Navigate(typeof(View.NovelDetails), e.ClickedItem);
+        }
+    }
+}
---
+++ JNCReaderUWP/ViewModel/API/SeriesViewModel.cs
@@ -0,0 +1,49 @@
+﻿using JNovelClub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JNCReaderUWP.ViewModel
+{
+    public class SeriesViewModel
+    {
+        public SeriesViewModel(Series s)
+        {
+            Title = s.Title;
+            Description = s.Description;
+            OriginalTitle = s.TitleOriginal;
+            IllustratedBy = $"{s.Illustrator} ({s.IllustratorOriginal})";
+            Author = $"{s.Author} ({s.AuthorOriginal})";
+            TranslatedBy = s.Translator;
+            ShortDescription = s.DescriptionShort;
+            CoverUrl = new Uri(new Uri("https://s3.amazonaws.com/production.j-novel.club/"), s.Attachments.FirstOrDefault()?.FullPath).AbsoluteUri;
+            Tags = s.Tags;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string OriginalTitle { get; set; }
+        public string TranslatedBy { get; set; }
+        public string IllustratedBy { get; set; }
+        public string Author { get; set; }
+        public string ShortDescription { get; set; }
+        public string CoverUrl { get; set; }
+        public string Tags { get; set; }
+
+        public KeyValueViewModel[] Properties { get
+            {
+                return new KeyValueViewModel[]
+                {
+                    new KeyValueViewModel("Original Title", OriginalTitle),
+                    new KeyValueViewModel("Author", Author),
+                    new KeyValueViewModel("Illustrator", IllustratedBy),
+                    new KeyValueViewModel("Translator", TranslatedBy),
+                    new KeyValueViewModel("Description", Description),
+                    new KeyValueViewModel("Tags", Tags),
+                };
+            }
+        }
+    }
+}
